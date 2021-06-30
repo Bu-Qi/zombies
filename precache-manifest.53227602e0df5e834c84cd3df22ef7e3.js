4d47637d68631bf1f4d5de4291d52d54000bb1ee
Bu-Qi <1204580150@qq.com>
2021-07-01 00:32:59 +0800

    Updates

---
+++ precache-manifest.53227602e0df5e834c84cd3df22ef7e3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e230a3604e452918f3fe597732d7e8e",
+    "url": "/zombies/index.html"
+  },
+  {
+    "revision": "2547c322ced35e93142c",
+    "url": "/zombies/static/css/main.5d0d477c.chunk.css"
+  },
+  {
+    "revision": "5d0254f78bcd9cf326df",
+    "url": "/zombies/static/js/2.0f49317d.chunk.js"
+  },
+  {
+    "revision": "6c19783ced6bae75444858dab9dff6e1",
+    "url": "/zombies/static/js/2.0f49317d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2547c322ced35e93142c",
+    "url": "/zombies/static/js/main.6922c2ef.chunk.js"
+  },
+  {
+    "revision": "8ffe6c4135178de6838e",
+    "url": "/zombies/static/js/runtime-main.f8657765.js"
+  },
+  {
+    "revision": "d2b2559b651e6a50f8d43f206dcdb8e7",
+    "url": "/zombies/static/media/Btn_Register_Normal.d2b2559b.png"
+  },
+  {
+    "revision": "d3684ace4f10c62c9209b45d3bb724c2",
+    "url": "/zombies/static/media/Btn_Register_Over.d3684ace.png"
+  },
+  {
+    "revision": "64a6f16c4f5ef7e189e75dfc0a52a25f",
+    "url": "/zombies/static/media/homecard@2x.64a6f16c.png"
+  },
+  {
+    "revision": "9d0437bbb5e705bad95ac929361faf3e",
+    "url": "/zombies/static/media/index_bg.9d0437bb.png"
+  },
+  {
+    "revision": "41236031fc933b053717ef1760601e36",
+    "url": "/zombies/static/media/start_btn.41236031.png"
+  },
+  {
+    "revision": "2a4a2235f6ed0e838b49a3f1bec4aae1",
+    "url": "/zombies/static/media/tester-bg@2x.2a4a2235.png"
+  },
+  {
+    "revision": "b102b8003aab95865cb7363428273a16",
+    "url": "/zombies/static/media/walls.b102b800.jpg"
+  },
+  {
+    "revision": "08087132d6dcfcd5c5f471880c104712",
+    "url": "/zombies/static/media/zombierun.08087132.png"
+  }
+]);
